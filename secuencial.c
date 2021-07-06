7a793f796d0c245593e53e8cb01a320a32655ce3
Andrea <andreamaldonadoma@gmail.com>
2021-07-06 13:17:15 -0300

    prueba2

--- secuencial.c
+++ secuencial.c
@@ -475,7 +475,7 @@ int main( int argc, char *argv[] ){
 	duracion= double (tiempo_final - tiempo_inicial); 
 	duracion/= CLOCKS_PER_SEC; 
 	printf("El tiempo %d es de: %ld segundos \n", tam, duracion);
-	
+	//hola 
 	printf("Time : %ld seconds.\n", tDuracion);
 	system("pause");
 	return 0;
