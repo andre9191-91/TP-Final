631f0181423e6a7c5ddf8adf6138828155e55523
Andrea <andreamaldonadoma@gmail.com>
2021-07-07 10:48:00 -0300

    prueba 4 en un unico archivo

--- ejecutar_todo_mpi.bash
+++ ejecutar_todo_mpi.bash
@@ -3,11 +3,14 @@
 #-------------------------------------------------------------
 #-------- ejecuta secuencial para los diferentes tamaños------
 # antes de ejecutar este script.. darle permisos:-------------
-#   chmod 777 ejecutar_todo_mpi
+#   chmod 777 ejecutar_todo_mpi.bash
 #-------------------------------------------------------------
 
-mpirun -np 4 -f ./hostfile ./mpi 200 2000  >> timempiT1.txt
-mpirun -np 4 -f ./hostfile ./mpi 800 1000  >> timempiT2.txt
-mpirun -np 4 -f ./hostfile ./mpi 1500 500 >> timempiT3.txt
+mpirun -np 4 -f ./hostfile ./mpi 200 2000  >> timeGobalmpi.txt
+mpirun -np 4 -f ./hostfile ./mpi 800 1000  >> timeGlobalmpi.txt
+mpirun -np 4 -f ./hostfile ./mpi 1500 500 >> timeGlobalmpi.txt
+mpirun -np 4 -f ./hostfile ./mpi 200 2000  >> timeGlobalmpi.txt
+
+
 
 # gdialog --msgbox "Listo! se ejecutaron los procesos, la salidas se encuentran en los archivos que correspondan"
